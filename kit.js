6532e70a4536148dfbc4dd665a6d8e87eced3ae3
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2024-03-08 00:39:45 +0900

    Update kit.js

--- kit.js
+++ kit.js
@@ -1,26 +1,52 @@
 let windows=[]
+let automovables=[]
 // ステップ実行用の関数を定義 //
-function o(id,asNum,x,y) { //Open:ウィンドウを開く
+function o(id, asNum, x, y, autoMoveAction) { //Open:ウィンドウを開く
     windows[asNum]=window.open(`show.html?id=${id}`, "", `left=${x}, top=${y}, width=500, height=720`); //TODO:相対的な表示サイズ、座標を自動的に決定するようにする
+    switch(autoMoveAction){
+        case 1:
+            automovables.push(asNum);
+            break;
+        case 2:
+            moveAutomove();
+            break;
+        case 3:
+            moveAutomove();
+            automovables.push(asNum);
+            break;
+    }
 }
 
-function c(asNums) { //Close:ウィンドウを閉じる
-    //引数に数値型が与えられた場合、配列に変換する
-    if (typeof asNums === "number") {
-        asNums=[asNums];
+function c(asNum0, asNum1) { //Close:ウィンドウを閉じる
+    //asNum1が与えられなかったとき、asNum0の内容をコピーする
+    if (typeof asNum1 === "undefined") {
+        asNum1=asNum0;
     }
     //ウィンドウを閉じる
-    asNums.forEach((asNum)=>{
-        windows[asNum].close();
-    })
+    for (i=asNum0; i<=asNum1; i++) {
+        windows[i].close();
+    }
+    //自動移動登録を解除
+    automovables=[]
 }
 
-function m(asNum) { //Move:ウィンドウを左方向に移動
-    windows[asNum].moveBy(-75, 0); //TODO:相対的な移動距離を自動的に決定するようにする
+function m(asNum0, asNum1) { //Move:ウィンドウを左方向に移動
+    //asNum1が与えられなかったとき、asNum0の内容をコピーする
+    if (typeof asNum1 === "undefined") {
+        asNum1=asNum0;
+    }
+    //ウィンドウを移動
+    for (i=asNum0; i<=asNum1; i++) {
+        windows[i].moveBy(-70, 0); //TODO:相対的な移動距離を自動的に決定するようにする
+    }
 }
 
 function t(asNum) { //Top:ウィンドウを最前面にする
     windows[asNum].focus();
+}
+
+function r(asNum, id) { //Rerender:既存のウィンドウを再描画する
+    windows[i].location.href=`show.html?id=${id}`
 }
 
 async function w(time) { //Wait:音源が指定時間になるまで待機
@@ -32,4 +58,11 @@ async function w(time) { //Wait:音源が指定時間になるまで待機
             };
         }, 15);
     });
+}
+
+// 自動移動処理 //
+function moveAutomove() {
+    automovables.forEach((i) => {
+        windows[i].moveBy(-75, 0);
+    })
 }
